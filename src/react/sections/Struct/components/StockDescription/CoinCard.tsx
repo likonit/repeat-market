fac570befd2b2df35e344df50a3a442053244961
unknown <likonitik@mail.ru>
2025-03-27 14:15:54 +0300

    add coin stock list preview

---
+++ src/react/sections/Struct/components/StockDescription/CoinCard.tsx
@@ -0,0 +1,31 @@
+import { StockCoinInfo } from "@/scripts/stock/createStock";
+import * as style from "@/styles/Struct/Stock/stocklist.module.css";
+
+export default function StockCoinCard({ coin }: { coin: StockCoinInfo }) {
+    return (
+        <div className={style.stocklist__coinCard}>
+            <div>
+                <img
+                    alt={coin.symbol}
+                    width={50}
+                    height={50}
+                    src={coin.imageLink}
+                ></img>
+            </div>
+            <div>
+                <p>{coin.name}</p>
+                <p>{coin.symbol}</p>
+                <p>{coin.usdValue.toFixed(2)} $</p>
+                <p>
+                    {coin.coinValue.toFixed(10)} {coin.symbol}
+                </p>
+            </div>
+            <div>
+                <p>{(coin.weight * 100).toFixed(2)}%</p>
+            </div>
+            <div>
+                <p>{coin.APR.toFixed(2)}% APR </p>
+            </div>
+        </div>
+    );
+}
