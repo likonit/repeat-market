afb011ebcad5a6c298e221f598b87358e25595b4
unknown <likonitik@mail.ru>
2025-04-05 12:18:58 +0300

    remove gh pages

--- src/scripts/api/loadCoins.ts
+++ src/scripts/api/loadCoins.ts
@@ -5,7 +5,7 @@ export default async function loadCoins(
 ): Promise<CoinRowInfo[]> {
     try {
         const fetchResult = await fetch(
-            `https://45.155.207.9:5132/crypto?limit=${limit}`
+            `http://45.155.207.9:5132/crypto?limit=${limit}`
         );
         const jsonData: { status: any; data: any[] } = await fetchResult.json();
         const rows: CoinRowInfo[] = jsonData.data.map((item, i) => {
